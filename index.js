46232617693ca938490a5e7efc088161ccb6a9b1
Newton <newton.bundi@student.moringaschool.com>
2023-07-18 22:17:08 +0300

    Assignement Completed

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = function(number1=2000,number2=100){
+    return (number1/number2);
+}
+
+divide();
+
+const square = x => (x**2);
+
+const add = (a,b) => (a+b);
+
+square(2);
+
+add(3,4);
